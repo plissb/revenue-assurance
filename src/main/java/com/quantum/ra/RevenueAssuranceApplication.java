55a979cf5fc1f72c5607233039088f4241ebc584
Boris Pliss <boris.pliss@quantum-soft.dev>
2025-04-08 11:03:56 +0300

    init commit

---
+++ src/main/java/com/quantum/ra/RevenueAssuranceApplication.java
@@ -0,0 +1,13 @@
+package com.quantum.ra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevenueAssuranceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RevenueAssuranceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/quantum/ra/RevenueAssuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quantum.ra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevenueAssuranceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
